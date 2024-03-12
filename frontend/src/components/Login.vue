c8885433bae63ff7f99eb1f079bafb66d45eae3d
Mach1r0 <segundacontafd@gmail.com>
2024-03-11 22:13:09 -0300

    Styling Login Page

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,34 +1,53 @@
 <template>
-    <div class="title-style">
-        <h1> Login page </h1>
-    </div>
-    <div class="container-login">
-        <input type="text" placeholder="Nickname or email">
-        <input type="text" placeholder="password">
-        <router-link to="/forgot-password">Forgot the password?</router-link>
-    </div>
-    <button class="btnlogin"> Login</button>
+  <div class="title-style">
+    <h1>Login page</h1>
+  </div>
+  <div class="container-login">
+    <input type="text" placeholder="Nickname or email" />
+    <input type="text" placeholder="password" />
+    <router-link to="/forgot-password">Forgot the password?</router-link>
+  </div>
+  <button class="btnlogin">Login</button>
 </template>
 
 <script>
 export default {
-    name: 'LoginPage'
-}
+  name: "LoginPage",
+};
 </script>
 
 <style>
-*{
-    margin: 0;
-    width: 100%;
-    
-}
 
-.container-login{
-    display: block;
-    justify-content: center;
-    align-items: center;
-    margin-left: auto;
-    margin-right: auto;
-    background-color: red;
+* {
+  margin: 0 auto;
+  width: 100%;
+}
+.container-login input{
+    justify-content: space-between;
+    margin: 10px;
+    height: 30px;
+    border-radius: 3px;
+    ]
+
+
+
+
+    
+
+
+}
+.container-login {
+  display: block;
+  justify-content: center;
+  align-items: center;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 400px;
+  padding: 0 0 0 40px;
+  background-color: red;
+}
+.btnlogin {
+    max-width: 300px;
+
 }
 </style>
