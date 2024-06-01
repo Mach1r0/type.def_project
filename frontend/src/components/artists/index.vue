cee15e300b53913dfbce31df9ad6e8746fb397e4
Mach1r0 <segundacontafd@gmail.com>
2024-06-01 00:00:57 -0300

    changing the style of navbar and the artist page

--- frontend/src/components/artists/index.vue
+++ frontend/src/components/artists/index.vue
@@ -1,7 +1,7 @@
 <template>
   <AppNavBar></AppNavBar>
     <section>
-      <div v-for="(artist, index) in artists" :key="index" class="container-album">
+      <div class="container-album" v-for="(artist, index) in artists" :key="index">
         <div class="img-artista">
           <img :src="artist.image" :alt="artist.name">
         </div>
@@ -12,7 +12,7 @@
             </router-link>
             <p>{{ artist.genders }}</p>
           </div>
-          <button>Show albuns</button>
+          <button class="btn-albuns">Show albuns</button>
         </div>
       </div>
     </section>
@@ -45,50 +45,82 @@ export default {
 </script>
 <style>
 
-*{
-  width: 100vw; 
-}
-
 section {
-  background-color: pink;
+  width: 100%;
+  height: 100vh;
+  background-color: #0b1528;
   display: flex;
   flex-direction: column; 
-  gap: 1rem;
+  gap: 0;
+  align-items: center;
 }
 
 .container-album {
-  border: 1px solid black;
+  border: 1px solid #ddd;
   padding: 10px;
-  background-color: antiquewhite;
+  background-color: #152238;
   display: flex;
+  margin-left: 90px;
+  border-radius: 20px;
+  max-width: 700px;
+  max-height: 300px ;
+  margin-top: 20px;
+  width: 90%;
+  box-shadow: 0 2px 4px rgba(0,0,0,0.1);
 }
 
 .img-artista {
   width: 150px;
   height: 150px;
+  flex-shrink: 0;
 }
 
 .img-artista img {
   width: 100%;
   height: 100%;
-  border-radius: 20px;
-  background-color: red;
+  border-radius: 5px;
+  object-fit: cover;
 }
 
 .descricao {
-  width: calc(100% - 150px);
-  background-color: rgb(149, 100, 194);
-  text-align: start;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  align-items: flex-start; 
 }
 
 .texto {
-  padding: 5px;
+  font-size: 18px;
+  margin-left: -70px; 
+}
+
+.texto h3 {
+  margin-bottom: 10px;
+}
+
+.artist-name{
+  text-decoration: none;
+  display: flex;
+  color: #84C5FB;
 }
 
 button {
-  width: 100%;
+  font-size: 16px;
+  margin-left: -70px;
+  border: none;
+  max-width: 600px;
+  border-radius: 5px;
+  background-color: #333;
+  color: #fff;
+  cursor: pointer;
+  align-self: flex-start; 
 }
+@media (max-width: 600px) {
+  button {
+    padding: 5px 10px;
+    font-size: 12px;
+    max-width: 300px;
+  }
+}
+
 </style>
--- frontend/src/components/base/navbar.vue
+++ frontend/src/components/base/navbar.vue
@@ -3,20 +3,20 @@
     <div class="head">
       <div class="h-left">
         <router-link to='/' class="title-class">TYPE.DEF</router-link>
-        <div class="h-search">
-          <input type="text" v-model="searchQuery" placeholder="Search">
-          <i class="fas fa-search"></i>
-        </div>
-      </div>
-      <div class="h-right">
-        <router-link to="/sign-up">Register</router-link>
-        <router-link class="login-css" to="/login">Login</router-link>
       </div>
     </div>
     <div class="head-links">
       <router-link to="/gender">Genres</router-link>
       <router-link to="/artist">Artists</router-link>
       <router-link to="/albums">Albums</router-link>
+    </div>
+    <div class="h-search">
+      <input type="text" v-model="searchQuery" placeholder="Search">
+      <i class="fas fa-search"></i>
+    </div>
+    <div class="h-right">
+      <router-link class="signup-css" to="/sign-up">Register</router-link>
+      <router-link class="login-css" to="/login">Login</router-link>
     </div>
   </header>
 </template>
@@ -33,103 +33,130 @@ export default {
 </script>
 
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
-    * {
-        padding: 0;
-        margin: 0;
-        box-sizing: border-box;
-        font-family: "Inter", sans-serif;
-    }
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
 
-    header {
-        display: flex;
-        flex-direction: column;
-    }
-    .title-class{
-      color: black;
-      text-decoration: none;
-      font-size: 2rem;
-      font-weight: bolder;
-      margin-left: 10px;
-      gap: 1rem;
-      padding: 20px;
-    }
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: "Inter", sans-serif;
+}
 
-    .head {
-        display: flex;
-        width: 100%;
-        justify-content: space-between;
-        align-items: center;
-    }
-    .h-left {
-        width: 50%;
-        display: flex;
-        align-items: center;
-    }
-    .h-left h1 {
-        padding: 30px;
-    }
-    .h-search {
-        display: flex;
-        align-items: center;
-        background-color: #FFFFFF;
-        height: 40px;
-        border-radius: 7px;
-        box-shadow: -1px 4px 5px 0px rgba(0,0,0,0.75);
-    }
-    .h-search input {
-        width: 100%;
-        height: 100%;
-        outline: none;
-        border: none;
-        border-radius: 7px;
-        padding: 10px;
-        font-size: 16px;
-        font-weight: bolder;
-        background-color: transparent;
-    }
-    .h-search i {
-        padding: 10px;
-        color: #cccccc;
-        background-color: transparent;
-    }
+header {
+    display: flex;
+    max-height: 46px; /* Adjusted this line */
+    background-color: #152238;
+    border-bottom: 2px solid #fff; 
+    align-items: center;
+    margin-left: 0px;
+}
 
-    .h-right {
-        display: flex;
-        align-items: center;
-        gap: 2rem;
-        padding: 30px;
-    }
-    .h-right a {
-        text-decoration: none;
-        color: black;
-        font-weight: bolder;
-    }
-    .login-css {
-        padding: 10px;
-        border: 1px solid black;
-        border-radius: 8px;
-    }
+.title-class{
+  color: #fff;
+  text-decoration: none;
+  font-size: 2rem;
+  font-weight: bolder;
+  margin-left: -120px; /* Decreased from 10px to 0px */
+  font-size: 1.5rem;
+  padding: 10px;
+}
 
-    .head-links {
-        width: 100%;
-        display: flex;
-        justify-content: center;
-        gap: 4rem;
-        padding-top: 10px;
-        border-bottom: 2px solid #cccccc;
-    }
-    .head-links a {
-        text-decoration: none;
-        color: black;
-        font-weight: bolder;
-        font-size: 16px;
-        border-radius: 12px 12px 0 0;
-        padding: 7px;
-    }
-    .head-links a:hover {
-        background-color: #cccccc;
-    }
-    
+.head {
+    display: flex;
+    width: 100%;
+    justify-content: flex-start;
+    align-items: center;
+}
+.h-left {
+    width: 30%;
+    display: flex;
+    align-items: center;
+    border-right: 2px solid black;
+    padding-left: 120px; /* Decreased from 10px to 5px */
+}
+
+.h-search {
+    display: flex;
+    align-items: center;
+    background-color: #fff;
+    height: 30px;
+    width: 400px;
+    width: 100%;
+    border-radius: 7px;
+    font-size: 14px;
+    padding: 5px;
+}
+
+.h-search input {
+    width: 100%;
+    height: 100%;
+    outline: none;
+    border: none;
+    border-radius: 7px;
+    padding: 10px;
+    font-size: 16px;
+    font-weight: bolder;
+    background-color: transparent;
+}
+
+.h-search i {
+    padding: 10px;
+    color: #333;
+    background-color: transparent;
+}
+
+.h-right {
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+    font-size: 16px;
+    margin-top: 10px;
+}
+
+.h-right a {
+    padding: 8px; 
+    text-decoration: none;
+    color: #fff;
+    font-weight: bolder;
+}
+
+.login-css, .signup-css {
+    border-radius: 12px 12px 0 0; 
+    color: #fff;
+    transition: color 0.3s ease; 
+}
+
+.login-css:hover {
+    color: black;
+}
+
+.login-css:hover, .signup-css:hover {
+    color: #333;
+    background-color: #fff;
+}
+
+.head-links {
+  width: 50%; 
+  display: flex;
+  justify-content: flex-start;
+  gap: 4rem;
+  padding-top: 10px;
+  border-right: 2px solid #fff; 
+  margin-left: -150px; /* Add this line */
+  margin-right: 20px; /* Add this line */
+}
+.head-links a {
+    text-decoration: none;
+    color: #fff;
+    font-weight: bolder;
+    font-size: 16px;
+    border-radius: 12px 12px 0 0;
+    padding: 7px;
+}
+
+.head-links a:hover  {
+    background-color: #fff;
+    color: #333;
+}
 </style>
 
