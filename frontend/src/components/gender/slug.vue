d1e98bbef83d1cd8752c44693c434ca54385a2ab
mach1r0 <segundacontafd@gmail.com>
2024-06-07 10:33:08 -0300

    adding style for genrer slug page

--- frontend/src/components/gender/slug.vue
+++ frontend/src/components/gender/slug.vue
@@ -1,17 +1,18 @@
 <template>
     <AppNavBar/>
-    <div class="container-allgenres">
-        <li v-for="(gender, index) in gender" :key="index">
-        <div class="container-image"> 
-            <p>
-                gender
-            </p>
+    <div v-if="gender" class="container-all"> 
+        <div class="container-header">
+            Top albums of {{ gender.name }}
         </div>
-        <div class="container-title"> 
-            titulo
-         </div>
-        <div class="container-artist"> artist </div>
-        </li>
+
+        <div class="container-body">
+            <div class="container-image">
+
+            </div>
+            <div class="bio-container">
+                
+            </div>                 
+        </div>
     </div> 
 
 </template>
@@ -29,21 +30,66 @@ export default {
             gender: null
         }
     }, 
-    async created(){
-        try{
-            const response = await axios.get("http://localhost:8000/gender")
-            this.gender = response.data
-            
-            this.gender.forEach(gender => {
-                console.log('data')
-            });
-        } catch (error) {
-            console.error('Failed to fetch data of gender: ', error)
-        }
+async created(){
+    try{
+        const slug = this.$route.params.slug;
+        const response = await axios.get(`http://localhost:8000/genders/${slug}`);
+        this.gender = response.data;
+        
+        this.gender.forEach(gender => {
+            console.log('data');
+        });
+    } catch (error) {
+        console.error('Failed to fetch data of gender: ', error);
     }
+}
 }
 
 </script>
 
 <style>
+*{
+    margin: 0;
+    padding: 0;
+}
+
+.container-all{
+    display: flex;
+    width: 100%;
+    height: 100vh;
+    background-color: aqua;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.container-header{
+    display: flex;
+    width: 60%;
+    background-color: brown;
+    height: 20%;
+}
+
+.container-body{
+    display: flex;
+    width: 60%;
+    height: 80%;
+    background-color: grey;
+}
+
+
+.container-image{
+    display: flex;
+    height: 30%;
+    width: 20%;
+    background-color: cornflowerblue;
+}
+
+.bio-container{
+    display: flex;
+    width: 80%;
+    height: 40%;
+    background-color: chartreuse;
+}
+
 </style>
